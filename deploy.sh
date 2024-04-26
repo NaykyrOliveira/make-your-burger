6e449f5ded112fdbdd882d393ca9eb7d567cb2cc
Naykyr Oliveira <naykyr@hotmail.com>
2024-04-26 10:18:53 -0300

    deploy

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+set -e
+
+npm run build 
+
+cd dist
+
+git init
+
+git add -A
+
+git commit -m 'deploy'
+
+git push -f git@github.com:https://github.com/NaykyrOliveira/make-your-burger.git main:gh-pages
+
+cd -
